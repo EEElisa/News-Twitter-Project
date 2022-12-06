2a39f0c5707b216339fe0a525bee4cffb2566be6
EEElisa <elisazmq@umich.edu>
2022-12-06 15:52:45 -0500

    initial commit

--- cache_Tweets.py
+++ cache_Tweets.py
@@ -19,10 +19,6 @@ access_token = TwitterSecrets.Access_Token
 access_token_secret = TwitterSecrets.Access_Token_Secret
 
 # %%
-search_url = "https://api.twitter.com/2/tweets/search/recent"
-query_params = {'query': '#MarchMadness'}
-
-# %%
 
 
 def bearer_oauth(r):
@@ -37,7 +33,7 @@ def bearer_oauth(r):
 # %%
 
 
-def open_cache():
+def open_cache(CACHE_FILENAME):
     ''' opens the cache file if it exists and loads the JSON into
     a dictionary, which it then returns.
     if the cache file doesn't exist, creates a new cache dictionary
@@ -56,9 +52,6 @@ def open_cache():
     except:
         cache_dict = {}
     return cache_dict
-
-# %%
-
 
 def save_cache(cache_dict):
     ''' saves the current state of the cache to disk
@@ -79,24 +72,86 @@ def save_cache(cache_dict):
 # %%
 
 
-def connect_to_endpoint(url, params):
+def make_request(url, params):
     response = requests.get(url, auth=bearer_oauth, params=params)
     print(response.status_code)
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
 
+def construct_unique_key(baseurl, params):
+    ''' constructs a key that is guaranteed to uniquely and 
+    repeatably identify an API request by its baseurl and params
+    Parameters
+    ----------
+    baseurl: string
+        The URL for the API endpoint
+    params: dictionary
+        A dictionary of param: param_value pairs
+    Returns
+    -------
+    string
+        the unique key as a string
+    '''
+    param_strings = []
+    connector = '_'
+    for k in params.keys():
+        param_strings.append(f'{k}_{params[k]}')
+    param_strings.sort()
+    unique_key = baseurl + connector +  connector.join(param_strings)
+    return unique_key
+
+def make_request_with_cache(baseurl, params):
+    '''Check the cache for a saved result for this baseurl+params
+    combo. If the result is found, return it. Otherwise send a new 
+    request, save it, then return it.
+    Parameters
+    ----------
+    baseurl: string
+        The URL for the API endpoint
+    params: dictionary
+        A dictionary of param: param_value pairs
+    Returns
+    -------
+    string
+        the results of the query as a Python object loaded from JSON
+    '''
+    request_key = construct_unique_key(baseurl, params)
+    if request_key in CACHE_DICT.keys():
+        print("cache hit!", request_key)
+        json_response = CACHE_DICT[request_key]
+        tweets_dic = json_response['data']
+        tweets_lst = []
+        for t in tweets_dic:
+            user_id = t['id']
+            text = t['text']
+            tweet = Tweet(user_id, text)
+            tweets_lst.append(tweet)
+        return tweets_lst
+    else:
+        print("cache miss!", request_key)
+        CACHE_DICT[request_key] = make_request(baseurl, params)
+        save_cache(CACHE_DICT)
+        json_response = CACHE_DICT[request_key]
+        tweets_dic = json_response['data']
+        tweets_lst = []
+        for t in tweets_dic:
+            tweet_id = t['id']
+            text = t['text']
+            tweet = Tweet(tweet_id, text)
+            tweets_lst.append(tweet)
+        return tweets_lst
 
 # %%
 class Tweet:
 
-    def __init__(self, user_id="No ID", text="No Text", json=None):
-        self.user_id = user_id
+    def __init__(self, tweet_id="No ID", text="No Text", json=None):
+        self.tweet_id = tweet_id
         self.text = text
         self.json = json
 
         if self.json != None:
-            self.user_id = json['user_id']
+            self.tweet_id = json['tweet_id']
             self.text = json['text']
 
     def get_tweet(self):
@@ -104,11 +159,17 @@ class Tweet:
 
 
 def main():
-    json_response = connect_to_endpoint(search_url, query_params)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    # %%
+    search_url = "https://api.twitter.com/2/tweets/search/recent"
+    query_params = {'query': '#MarchMadness'}
+
+    tweets = make_request_with_cache(search_url, query_params)
+
+    print(tweets[0].text)
+    # tweets = json_response['data']
+
+    # print(json.dumps(json_response, indent=4, sort_keys=True))
 
 
 if __name__ == "__main__":
     main()
-
-# %%
